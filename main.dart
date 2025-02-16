64cee564688114faa0aa8dcfaf99d0bb2a653ee2
TaynaraTech <nascimento.taynaracristina@gmail.com>
2025-02-16 18:19:29 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,59 +1,76 @@
-import 'dart:async';
-import 'package:myapp/modelos/planeta.dart' show Planeta;
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
+import 'modelo/planeta.dart';
 
-class ControlePlaneta {
-  static Database? _bd;
+void main() {
+  runApp(const MyApp());
+}
 
-  Future<Database> get bd async {
-    if (_bd != null) return _bd!;
-    _bd = await _initBD('planetas.db');
-    return _bd!;
-  }
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  Future<Database> _initBD(String localArquivo) async {
-    final caminhoBD = await getDatabasesPath();
-    final caminho = join(caminhoBD, localArquivo);
-    return await openDatabase(caminho, version: 1, onCreate: _criarBD);
-  }
-
-  Future<void> _criarBD(Database bd, int versao) async {
-    const sql = '''
-  CREATE TABLE planetas ()
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  nome TEXT NOT NULL,
-  tamanho REAL NOT NULL,
-  distancia REAL NOT NULL,
-  apelido TEXT
-);
-''';
-    await bd.execute(sql);
-  }
-
-  Future<List<Planeta>> lerPlanetas() async {
-    final db = await bd;
-    final resultado = await db.query('planetas');
-    return resultado.map((item) => Planeta.fromMap(item)).toList();
-  }
-
-  Future<int> inserirPlaneta(Planeta planeta) async {
-    final db = await bd;
-    return await db.insert('planetas', planeta.toMap());
-  }
-
-  Future<int> alterarPlaneta(Planeta planeta) async {
-    final db = await bd;
-    return await db.update(
-      'planetas',
-      planeta.toMap(),
-      where: 'id = ?',
-      whereArgs: [planeta.id],
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Planetas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'CRUD - Planetas'),
     );
   }
+}
 
-  Future<int> excluirPlaneta(int id) async {
-    final db = await bd;
-    return await db.delete('planetas', where: 'id = ?', whereArgs: [id]);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  final Planeta _planeta = Planeta(
+    nome: 'Terra',
+    tamanho: 100.0,
+    distancia: 1000.0,
+  );
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      _planeta.tamanho = _planeta.tamanho * 2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '${_planeta.nome} - ${_planeta.tamanho} - ${_planeta.distancia},
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
